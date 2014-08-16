1c38308c9bccb8de1e50c07cb93a829f84980598
janardhanpshetty <janardhanp22@gmail.com>
2014-08-16 11:08:21 -0700

    Working code on hadoop cluster

--- hdfsfilecount.py
+++ hdfsfilecount.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 ''' Function to get the filecounts of hdfs using hadoop shell commands'''
-
 from subprocess import Popen,PIPE
 def get_file_counts_hdfs_day(hdfspath):
-  phadoopcount = Popen(["hadoop","fs","-count","/shared/app_logs"],stdout=PIPE)
+  phadoopcount = Popen(["hadoop","fs","-count",hdfspath],stdout=PIPE)
   countoutput=phadoopcount.communicate()[0]
   filecount=countoutput.split()[1]
   return filecount
-  
+
+if __name__=='__main__': 
+  filecounts=get_file_counts_hdfs_day('/shared/app_logs')
+  print filecounts
